b1667fc2b4dba17b0999d243e5bd9cfafd5c6acc
Gavin Matthews <gmatthews@axway.com>
2019-02-21 10:15:42 +0000

    Cleanup

--- perf.go
+++ perf.go
@@ -13,39 +13,46 @@ func now() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
 }
 
+func oneget(client *http.Client, id string, config *Config) {
+	req, _ := http.NewRequest("GET", config.URL, nil)
+
+	// Add the APIKey if there is one
+	if len(config.APIKey) > 0 {
+		req.Header.Set("Authorization", "Apikey "+config.APIKey)
+	}
+
+	// Add additional headers
+	for h := range config.Headers {
+		header := strings.SplitN(config.Headers[h], ":", 2)
+		req.Header.Set(header[0], header[1])
+	}
+
+	start := now()
+	res, err := client.Do(req)
+	end := now()
+
+	defer res.Body.Close()
+
+	if err != nil {
+		fmt.Printf("%s: (%d) %s\n", id, end-start, err)
+	} else {
+		status := res.StatusCode
+		reqID := res.Header.Get("request-id")
+		// if len(reqID) == 0 {
+		// 	bodyBytes, _ := ioutil.ReadAll(res.Body)
+		// 	bodyString := string(bodyBytes)
+		// 	fmt.Printf("%+v\n", bodyString)
+		// }
+		fmt.Printf("%s: (%d) %d %s\n", id, end-start, status, reqID)
+	}
+}
+
 func httpget(wg *sync.WaitGroup, id string, config *Config) {
 	defer wg.Done()
 	client := &http.Client{}
 
 	for i := 0; i < config.Iterations; i++ {
-		req, _ := http.NewRequest("GET", config.URL, nil)
-
-		// Add the APIKey if there is one
-		if len(config.APIKey) > 0 {
-			req.Header.Set("Authorization", "Apikey "+config.APIKey)
-		}
-		// Add additional headers
-		for h := range config.Headers {
-			header := strings.SplitN(config.Headers[h], ":", 2)
-			req.Header.Set(header[0], header[1])
-		}
-
-		start := now()
-		res, err := client.Do(req)
-		end := now()
-
-		if err != nil {
-			fmt.Printf("%s-%d: (%d) %s\n", id, i, end-start, err)
-		} else {
-			status := res.StatusCode
-			reqID := res.Header.Get("request-id")
-			// if len(reqID) == 0 {
-			// 	bodyBytes, _ := ioutil.ReadAll(res.Body)
-			// 	bodyString := string(bodyBytes)
-			// 	fmt.Printf("%+v\n", bodyString)
-			// }
-			fmt.Printf("%s-%d: (%d) %d %s\n", id, i, end-start, status, reqID)
-		}
+		oneget(client, fmt.Sprintf("%s-%d", id, i), config)
 	}
 }
 
